4bdf2c455c0b3268541b348991ec4d06be639164
Mark Feldman <mefeldman_1@edu.hse.ru>
2025-03-24 18:21:32 +0300

    scroll up arrow

--- src/components/shared/ScrollUp/ScrollUp.js
+++ src/components/shared/ScrollUp/ScrollUp.js
@@ -1,16 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import s from './ScrollUp.module.css'
 import arrow from '@/static/icons/chevron-up.svg'
 import Image from "next/image";
+import cn from 'classnames'; // Импортируйте библиотеку 'classnames'
 
 const ScrollUp = () => {
+    const [visible, setVisible] = useState(false);
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+    const checkScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const visible = prevScrollPos > currentScrollPos;
+
+        setPrevScrollPos(currentScrollPos);
+        setVisible(visible);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', checkScroll);
+        return () => window.removeEventListener('scroll', checkScroll);
+    }, [prevScrollPos]);
+
     const click = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth',
         });
     }
+
     return (
-        <div className={s.scroll_btn} onClick={click}>
+        <div className={cn(s.scroll_btn, { [s.visible]: visible })} onClick={click}>
             <Image width={30} src={arrow} alt='' className={s.icon}/>
         </div>
     );
--- src/pages/brands.js
+++ src/pages/brands.js
@@ -1,5 +1,10 @@
 import MainLayout from "@/layout/MainLayout";
+import {fetchFilter, fetchProductsPage} from "@/http/productsApi";
 
+// export const getStaticProps = async () => {
+//     const brands = await fetchFilter('brands')
+//     return { props: {brands} }
+// }
 const Brands = () => {
     return (
         <MainLayout>
