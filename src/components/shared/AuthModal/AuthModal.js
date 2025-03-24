93d5effd940bcd13c22be6f572a831442c11715e
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-03-24 20:36:28 +0300

    brand page + bug fixing

--- src/components/shared/AuthModal/AuthModal.js
+++ src/components/shared/AuthModal/AuthModal.js
@@ -62,7 +62,7 @@ const AuthModal = ({children}) => {
             userStore.setFirstName(res.first_name)
             userStore.setLastName(res.last_name)
             userStore.setAccessToken(res.access_token)
-            localStorage.setItem('access_token', res.access_token)
+            // localStorage.setItem('access_token', res.access_token)
             setShow(false)
         } catch (e) {
             setEmailBusy(true)
@@ -86,7 +86,7 @@ const AuthModal = ({children}) => {
             userStore.setFirstName(res.first_name)
             userStore.setLastName(res.last_name)
             userStore.setAccessToken(res.access_token)
-            localStorage.setItem('access_token', res.access_token)
+            // localStorage.setItem('access_token', res.access_token)
             setShow(false)
         } catch (e) {
             setWrong(true)
