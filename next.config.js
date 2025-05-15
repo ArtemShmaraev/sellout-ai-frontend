47dfab67b2206bfc8c8dbc7a91b473b51776938b
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-05-15 16:49:04 +0300

    size tables on one product page and material filter started

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
-    domains: ['cdn.poizon.com', 'drive.google.com', 'storage.yandexcloud.net'],
+    domains: ['cdn.poizon.com', 'drive.google.com', 'storage.yandexcloud.net', 'img.stadiumgoods.com'],
   },
   experimental: {
     scrollRestoration: true,
