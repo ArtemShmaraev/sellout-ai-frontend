c2ccb93b60572d8e5bf4fde53834b43c02eb958e
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-05-19 19:42:32 +0300

    fixes

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -19,7 +19,10 @@ export const getServerSideProps = async (context) => {
     const cookies = parse(context.req.headers.cookie || '')
     const token = cookies['access_token']
     let productUnits
-    const cartArr = cookies['cart'].trim().split(' ').map(el => Number(el))
+    let cartArr
+    if (cookies.cart) {
+        cartArr = cookies['cart'].trim().split(' ').map(el => Number(el))
+    }
     if (cookies['cart']) {
         const obj = {
             product_unit_list: cartArr
