5eb3635cd1c6d623b6b5d921109fd7f95355ce83
Mark Feldman <mefeldman_1@edu.hse.ru>
2025-06-29 17:50:03 +0300

    payment

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -43,6 +43,35 @@ const MainLayout = ({children}) => {
             <Head>
                 <meta name={'description'} content={'Купить кроссовки круто'}/>
                 <meta name="theme-color" content="#000000"/>
+                <script
+                    type="text/javascript"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                  return;
+                }
+              }
+              k=e.createElement(t);
+              a=e.getElementsByTagName(t)[0];
+              k.async=1;
+              k.src=r;
+              a.parentNode.insertBefore(k,a);
+            })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(95264330, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `,
+                    }}
+                />
             </Head>
             <div className={'body'}>
                 <NavbarC/>
