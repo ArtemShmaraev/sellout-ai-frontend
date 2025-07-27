540085f11012a29f83fd3cba1733bed55ef7b2f2
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-07-27 16:57:29 +0300

    fixes

--- src/components/pages/oneProduct/ImgSlider/ImgSlider.js
+++ src/components/pages/oneProduct/ImgSlider/ImgSlider.js
@@ -41,7 +41,7 @@ const ImgSlider = ({photos}) => {
             >
                 {photos.map(el =>
                     <SwiperSlide className={s.photo}>
-                        <div className={s.photo_cont}>
+                        <div className={[s.photo_cont, 'swiper-zoom-container'].join(' ')}>
                             <Image src={el.url}
                                    alt={``}
                                    fill={true}
@@ -53,6 +53,65 @@ const ImgSlider = ({photos}) => {
                 )
                 }
             </Swiper>
+            {/*<Swiper*/}
+            {/*    style={{*/}
+            {/*        '--swiper-navigation-color': '#fff',*/}
+            {/*        '--swiper-pagination-color': '#fff',*/}
+            {/*    }}*/}
+            {/*    zoom={true}*/}
+            {/*    navigation={true}*/}
+            {/*    pagination={{*/}
+            {/*        clickable: true,*/}
+            {/*    }}*/}
+            {/*    modules={[Zoom, Navigation, Pagination]}*/}
+            {/*    className="mySwiper"*/}
+            {/*>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className="swiper-zoom-container">*/}
+            {/*            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*</Swiper>*/}
             {
                 isFull &&
                 <FullScreen toggle={toggleFullScreen} photos={photos}/>
