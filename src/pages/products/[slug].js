4107623fd92a1b93c818e6be409037b158ab06f2
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-09-13 21:49:33 +0300

    title

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -670,7 +670,7 @@ const OneProductPage = ({product, prices}) => {
                                             >
                                                 <div className={s.icon_block}>
                                                     <Image src={isInWishlist ? like_fill : like} alt=""
-                                                           className={s.icons} style={{ width: '20px', height: '20px' }}/>
+                                                           className={s.icons} style={{ width: '18px', height: '18px' }}/>
                                                     <div>{isInWishlist ? 'В избранном' : 'В избранное'}</div>
                                                 </div>
                                             </button>
@@ -678,7 +678,7 @@ const OneProductPage = ({product, prices}) => {
                                             <div className={s.fav_btn2}>
                                                 <AuthModal fromWishlist={true}>
                                                     <div className={s.icon_block}>
-                                                        <Image src={like} alt="" className={s.icons} style={{ width: '20px', height: '20px' }}/>
+                                                        <Image src={like} alt="" className={s.icons} style={{ width: '18px', height: '18px' }}/>
                                                         <div>В избранное</div>
                                                     </div>
                                                 </AuthModal>
