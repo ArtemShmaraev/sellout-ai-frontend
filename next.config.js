334b43d89773d1ecd0927da89ad0cfb35a61f863
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-10-28 15:28:00 +0300

    title

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
-    domains: ['cdn.poizon.com', 'drive.google.com', 'storage.yandexcloud.net', 'img.stadiumgoods.com'],
+    domains: ['cdn.poizon.com', 'drive.google.com', 'storage.yandexcloud.net', 'img.stadiumgoods.com', "console.cloud.yandex.ru", "bucket.sellout.su", "sellout.su"],
   },
   experimental: {
     scrollRestoration: true,
