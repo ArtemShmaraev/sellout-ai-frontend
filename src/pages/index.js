50c746a3503acbee071d1e63440cb1c4bdfe9958
Mark Feldman <mefeldman_1@edu.hse.ru>
2025-11-01 21:25:12 +0300

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,197 +19,40 @@ import man from "/src/static/img/man.png"
 import manBig from "/src/static/img/manBig.png"
 import mainbig from "/src/static/img/mainbig.png"
 import mainbigMob from "/src/static/img/Group 74.png"
-import {selectedGender, setSelectedGender} from "@/layout/MainLayout";
-import FirstMainBlock from "@/components/shared/UI/FirstMainBlock/FirstMainBlock";
-import ComplexMainPageBlock from "@/components/shared/UI/ComplexMainPageBlock/ComplexMainPageBlock";
 import {observer} from "mobx-react-lite";
 import {Context} from "@/context/AppWrapper";
 
 export const getServerSideProps = async (context) => {
-    const cookies = parse(context.req.headers.cookie || '')
-    const page = cookies['index_page']
-    const token = cookies['access_token']
 
-    const selected_gender = cookies['selected_gender']; // Добавляем получение выбранного гендера из кук
-
-    let data;
-
-    // Проверяем, выбран ли гендер
-    if (selected_gender) {
-        if (!page) {
-            // Добавляем передачу гендера в запрос на сервер
-            data = await fetchMainPage(token, false, true, 1, selected_gender);
-        } else {
-            // Аналогично, передаем гендер в запрос
-            data = await fetchMainPage(token, false, false, page, selected_gender);
-        }
-    } else {
-        // Если гендер не выбран, возвращаем пустые данные
-        data = [];
-    }
-    return {props: {data}};
+    return {props: {}};
 }
 const Home = ({data}) => {
-    const {desktopStore} = useContext(Context)
     const router = useRouter()
-    const [content, setContent] = useState(data)
     const [isDesktop, setIsDesktop] = useState(true)
-    const [showGenderModal, setShowGenderModal] = useState(false);
 
     useLayoutEffect(() => {
+        const savedGender = Cookies.get('selected_gender');
+        if (savedGender) {
+            if (savedGender == "M"){
+                router.push("/men")
+            } else {
+                router.push("/women")
+            }
+
+        }
         const checkIsDesktop = () => {
             const width = window.innerWidth;
             setIsDesktop(width > 1200);
         };
         checkIsDesktop();
 
-        window.addEventListener('resize', checkIsDesktop);
-        return () => {
-            window.removeEventListener('resize', checkIsDesktop);
-        };
-
     }, []);
 
 
-    useEffect(() => {
-        // Check if the gender is already selected in cookies
-        const savedGender = Cookies.get('selected_gender');
-        if (savedGender) {
-            // Gender is already selected, you can use it as needed
-            setSelectedGender(savedGender);
-            setShowGenderModal(false);
-        } else {
-            // Gender is not selected, show the gender selection modal
-            setShowGenderModal(true);
-        }
-
-        // Check if the user visited the page within the last 10 minutes
-        if (!Cookies.get('index_page')) {
-            const tenMinutes = new Date(new Date().getTime() + 10 * 60 * 1000);
-            Cookies.set('index_page', 1, {expires: tenMinutes});
-        }
-    }, []);
-
-    const handleGenderSelection = async (gender) => {
-        // Сохраняем выбранный гендер в куках
-        Cookies.set('selected_gender', gender, {expires: 2772});
-
-        // Отправляем запрос на сервер с выбранным гендером
-        const page = Cookies.get('index_page');
-        const token = Cookies.get('access_token');
-        const newData = await fetchMainPage(token, false, !page, page || 1, gender);
-
-        // Обновляем состояние компонента новыми данными
-        setContent(newData);
-
-        // Закрываем модальное окно
-        setShowGenderModal(false);
-    };
-
-    // useEffect(() => {
-    //     if (Cookies.get('selected_gender')) {
-    //         handleGenderSelection(Cookies.get('selected_gender'))
-    //     }
-    // }, [desktopStore.showGenderModal])
 
 
-    // Пустой массив зависимостей, чтобы useEffect вызывался только один раз
 
-    // useEffect(() => {
-    //     window.addEventListener("resize", checkIsDesktop);
-    //     // Call handler right away so state gets updated with initial window size
-    //     checkIsDesktop();
-    //     // Remove event listener on cleanup
-    //     return () => window.removeEventListener("resize", checkIsDesktop);
-    // })
 
-    // const [isPageReloaded, setIsPageReloaded] = useState(false);
-    //
-    // useEffect(() => {
-    //     // Проверяем, была ли страница перезагружена, проверяя, есть ли объект performance в браузере
-    //     if (typeof window !== 'undefined' && window.performance) {
-    //         const navigation = window.performance.getEntriesByType('navigation')[0];
-    //         if (navigation.type === 'reload') {
-    //             setIsPageReloaded(true);
-    //             const fiveHours = new Date(new Date().getTime() + 300 * 60 * 1000);
-    //             Cookies.set('index_page', 1, {expires: fiveHours})
-    //             router.push('/')
-    //         }
-    //     }
-    // }, []);
-    const getMore = async () => {
-        const token = Cookies.get('access_token')
-        const page = Cookies.get('index_page') ? Cookies.get('index_page') : 1
-        const tenMinutes = new Date(new Date().getTime() + 10 * 60 * 1000);
-        Cookies.set('index_page', Number(page) + 1, {expires: tenMinutes})
-        const gender = Cookies.get('selected_gender')
-        const newData = await fetchMainPage(token, true, false, Number(page) + 1, gender)
-        const arr = [...(content), ...newData]
-        setContent(arr)
-    }
-    const renderPage = () => {
-        const arr = []
-        content.forEach(el => {
-            if (el.type === 'firstMainBlockSTOPPED') {
-                arr.push(
-                    <FirstMainBlock obj={{
-                        "leftSmallVideo": el.leftSmallVideo,
-                        "rightSmallVideo": el.rightSmallVideo,
-                        "bigVideo": el.bigVideo
-                    }}/>
-                )
-            } else if (el.type === 'complexMainPageBlock') {
-                arr.push(
-                    <ComplexMainPageBlock obj={{
-                        "title": el.title,
-                        "fullWidthImage": el.fullWidthImage,
-                        "imagesInRowAmount": el.imagesInRowAmount,
-                        "imagesInRow": el.imagesInRow,
-                        "productsBlocks": el.productsBlocks,
-                        "productsSelection": el.productsSelection,
-                        "videosInRowAmount": el.videosInRowAmount,
-                        "videosInRow": el.videosInRow
-                    }}/>
-                )
-            } else if (el.type === 'photo') {
-                arr.push(
-                    <MainImgBlock obj={el.desktop} className={s.desktop}/>
-                )
-                arr.push(
-                    <MainImgBlock obj={el.mobile} className={s.mobile}/>
-                )
-            } else if (el.type === 'selection') {
-                const scrollableBlockArr = []
-                el.products.forEach(product => {
-                    scrollableBlockArr.push(
-                        <ProductCard
-                            product={product}
-                            key={product.id}
-                            smallCard={true}
-                        />
-                    )
-                })
-                arr.push(
-                    <div className={s.collections}>
-                        <div className='d-flex justify-content-between align-items-center my-5'>
-                            <div className={s.title_block}>
-                                <h3 className={s.title}>{el.title}</h3>
-                            </div>
-                            <div>
-                                <Link href={'/products?' + el.url} className={s.link}
-                                >{el.productsAmount ? <span>Все {el.productsAmount} моделей</span> :
-                                    <span>Посмотреть все</span>}</Link>
-                            </div>
-                        </div>
-                        <ScrollableBlock>
-                            {scrollableBlockArr}
-                        </ScrollableBlock>
-                    </div>
-                )
-            }
-        })
-        return arr
-    }
     const [isSend, setIsSend] = useState(false)
     const [show, setShow] = useState(false);
     const handleClose = () => {
