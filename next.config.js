8f2208f71cb6b870d3a5440b961978a3ce2685c5
Mark Feldman <mefeldman_1@edu.hse.ru>
2025-11-10 21:05:58 +0300

    title

--- next.config.js
+++ next.config.js
@@ -1,13 +1,50 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
-    domains: ['cdn.poizon.com', 'drive.google.com', 'storage.yandexcloud.net', 'img.stadiumgoods.com', "console.cloud.yandex.ru", "bucket.sellout.su", "sellout.su"],
+    domains: [
+      'cdn.poizon.com',
+      'drive.google.com',
+      'storage.yandexcloud.net',
+      'img.stadiumgoods.com',
+      'console.cloud.yandex.ru',
+      'bucket.sellout.su',
+      'sellout.su',
+    ],
   },
   experimental: {
     scrollRestoration: true,
   },
-}
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'cookie',
+            key: 'selected_gender',
+            value: 'M',
+          },
+        ],
+        permanent: false,
+        destination: '/men',
+      },
+      {
+        source: '/',
+        has: [
+          {
+            type: 'cookie',
+            key: 'selected_gender',
+            value: 'F',
+          },
+        ],
+        permanent: false,
+        destination: '/women',
+      },
+    ];
+  },
+  // Другие настройки, если есть
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
