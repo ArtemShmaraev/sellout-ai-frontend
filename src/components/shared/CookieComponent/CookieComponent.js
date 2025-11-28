e67503219f98932a70bcea25f5ffbb56a180b461
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-11-28 18:44:09 +0300

    новая версия

--- src/components/shared/CookieComponent/CookieComponent.js
+++ src/components/shared/CookieComponent/CookieComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import s from './CookieComponent.module.css'
+import Link from "next/link";
 
 const CookieComponent = ({isOpen, close}) => {
     return (
         isOpen &&
         <div className={s.modal}>
             <div className={s.text}>
-                Мы собираем Cookie.
+                <Link href={'/docs/Политика%20конфиденциальности.pdf'}
+                      target={'_blank'} style={{color: "white"}}>Мы собираем Cookie.</Link>
             </div>
             <button
                 className={s.btn}
