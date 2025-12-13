4c8945e7ff038fdbac8cd3fcebf83f6ac9d20df6
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2025-12-13 14:33:53 +0300

    title

--- src/pages/order.js
+++ src/pages/order.js
@@ -276,7 +276,7 @@ const Order = ({addresses, defaultPrice, finalPrice, sale, userData, maxBonuses,
                             <input type="hidden" name="pstype" value='sbp_default' />
                             <input type="hidden" name="json" value='true' />
                             <input type="hidden" name="user_result_callback" value={`https://sellout.su/api/v1/order/fact_of_payment?id=${order.id}`} />
-                            <input type="hidden" name="cart" value={order.invoice_data} />
+                            <input type="hidden" name="cart" value={ order.invoice_data }/>
                             {/*<input type="submit" value="Перейти к оплате" />*/}
                         </form>
 
