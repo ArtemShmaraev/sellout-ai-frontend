ac1078873e62e70bedc2d30de56b525a2829f640
Шайхутдинов Руслан <57287494+Violonistx@users.noreply.github.com>
2026-02-19 16:10:35 +0300

    cart

--- src/pages/catalog/accessories_desktop_men.js
+++ src/pages/catalog/accessories_desktop_men.js
@@ -1,11 +1,11 @@
 import MainLayout from "@/layout/MainLayout";
-import styles from '@/styles/CatalogShoesDesktopMen.module.css'
+import styles from '@/styles/CatalogAccessoriesDesktopMen.module.css'
 import React, {useContext, useEffect, useRef, useState} from "react";
 import {observer} from "mobx-react-lite";
 import Image from "next/image";
 import arrow from "@/static/icons/arrowSlider.svg";
 
-const CatalogShoesDesktopMen = () => {
+const Accessories_desktop_men = () => {
     const linksBrands = [
         'https://sellout.su/brand1', 'https://sellout.su/brand2', 'https://sellout.su/brand3',
         'https://sellout.su/brand4', 'https://sellout.su/brand5', 'https://sellout.su/brand6',
@@ -16,7 +16,15 @@ const CatalogShoesDesktopMen = () => {
         'https://sellout.su/brand19', 'https://sellout.su/brand20', 'https://sellout.su/brand21',
         'https://sellout.su/brand22', 'https://sellout.su/brand23', 'https://sellout.su/brand24',
         'https://sellout.su/brand25', 'https://sellout.su/brand26', 'https://sellout.su/brand27',
-        'https://sellout.su/brand28'
+        'https://sellout.su/brand28', 'https://sellout.su/brand1', 'https://sellout.su/brand2', 'https://sellout.su/brand3',
+        'https://sellout.su/brand4', 'https://sellout.su/brand5', 'https://sellout.su/brand6',
+        'https://sellout.su/brand7', 'https://sellout.su/brand8', 'https://sellout.su/brand9',
+        'https://sellout.su/brand10', 'https://sellout.su/brand11', 'https://sellout.su/brand12',
+        'https://sellout.su/brand13', 'https://sellout.su/brand14', 'https://sellout.su/brand15',
+        'https://sellout.su/brand16', 'https://sellout.su/brand17', 'https://sellout.su/brand18',
+        'https://sellout.su/brand19', 'https://sellout.su/brand20', 'https://sellout.su/brand21',
+        'https://sellout.su/brand22', 'https://sellout.su/brand23', 'https://sellout.su/brand24',
+        'https://sellout.su/brand25', 'https://sellout.su/brand26'
     ];
 
     const textsBrands = [
@@ -25,7 +33,13 @@ const CatalogShoesDesktopMen = () => {
         '11000 лотов', '12000 лотов', '13000 лотов', '14000 лотов', '15000 лотов',
         '16000 лотов', '17000 лотов', '18000 лотов', '19000 лотов', '20000 лотов',
         '21000 лотов', '22000 лотов', '23000 лотов', '24000 лотов', '25000 лотов',
-        '26000 лотов', '27000 лотов', '28000 лотов'
+        '26000 лотов', '27000 лотов', '28000 лотов',
+        '1000 лотов', '2000 лотов', '3000 лотов', '4000 лотов', '5000 лотов',
+        '6000 лотов', '7000 лотов', '8000 лотов', '9000 лотов', '10000 лотов',
+        '11000 лотов', '12000 лотов', '13000 лотов', '14000 лотов', '15000 лотов',
+        '16000 лотов', '17000 лотов', '18000 лотов', '19000 лотов', '20000 лотов',
+        '21000 лотов', '22000 лотов', '23000 лотов', '24000 лотов', '25000 лотов',
+        '26000 лотов'
     ];
 
     const scrollableContainerRef = useRef(null);
@@ -80,19 +94,40 @@ const CatalogShoesDesktopMen = () => {
                 {/* Header */}
                 <div className={styles.header}>
                     <div className={styles.closeButton}>✕</div>
-                    <div className={styles.title}>ОБУВЬ</div>
+                    <div className={styles.title}>АКСЕССУАРЫ</div>
                     <div className={styles.viewAll}>ПОСМОТРЕТЬ ВСЕ 100'000+ МОДЕЛЕЙ</div>
+                </div>
+
+                {/* Categories */}
+                <div className={styles.categoriesSection}>
+                    <div className={styles.categoriesTitle}>КАТЕГОРИИ</div>
+                    <div className={styles.categoriesGrid}>
+                        {['Кепки', 'Очки', 'Панамы', 'Часы', 'Оправы', 'Collectibles', 'Ремни', 'Шапки', 'Шарфы', 'Кольца', 'Цепочки', 'Браслеты', 'Перчатки', 'Очешники', 'Духи', 'Косметика', 'Галстуки', 'Брелоки', 'Мячи', 'Мячи', 'Мячи', 'Чехлы', 'Для техники', 'Другие'].map((category, idx) => (
+                            <div key={idx} className={styles.categoryItem}>
+                                <Image
+                                    src={`/Images New Frontend/Desktop/Men/Products/Accessories/Categories/${idx + 1}.png?v=${Date.now()}`}
+                                    alt={category}
+                                    width={700}
+                                    height={700}
+                                    className={styles.categoryImage}
+                                />
+                                <div className={styles.categoryText}>
+                                    {category}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
 
                 {/* Popular Brands */}
                 <div className={styles.brandsSection}>
                     <div className={styles.brandsTitle}>ПОПУЛЯРНЫЕ БРЕНДЫ</div>
                     <div className={styles.brandsGrid} ref={scrollableContainerRef}>
-                        {Array.from({length: 28}).map((_, idx) => (
+                        {Array.from({length: 57}).map((_, idx) => (
                             <div key={idx} className={styles.brandCircle}>
                                 <div className={styles.circle}>
                                     <Image
-                                        src={`/Images New Frontend/Desktop/Men/Products/Shoes/Brands/${idx + 1}.png?v=${Date.now()}`}
+                                        src={`/Images New Frontend/Desktop/Men/Products/Accessories/Brands/${idx + 1}.png?v=${Date.now()}`}
                                         alt="Brand Image"
                                         className={styles.circleImage}
                                         width={700}
@@ -116,30 +151,9 @@ const CatalogShoesDesktopMen = () => {
                         </button>
                     }
                 </div>
-
-                {/* Categories */}
-                <div className={styles.categoriesSection}>
-                    <div className={styles.categoriesTitle}>КАТЕГОРИИ</div>
-                    <div className={styles.categoriesGrid}>
-                        {['Кроссовки', 'Лоферы', 'Шлёпки', 'Слипоны', 'Зимние кроссов..', 'Все ботинки', 'Мартинсы', 'Тимберленды', 'Сандалии', 'Пляжные санда..', 'Мюли'].map((category, idx) => (
-                            <div key={idx} className={styles.categoryItem}>
-                                <Image
-                                    src={`/Images New Frontend/Desktop/Men/Products/Shoes/Categories/${idx + 1}.png?v=${Date.now()}`}
-                                    alt={category}
-                                    width={700}
-                                    height={700}
-                                    className={styles.categoryImage}
-                                />
-                                <div className={styles.categoryText}>
-                                    {category}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
             </div>
         </MainLayout>
     );
 };
 
-export default observer(CatalogShoesDesktopMen);
+export default observer(Accessories_desktop_men);
--- src/pages/catalog/bags_desktop_women.js
+++ src/pages/catalog/bags_desktop_women.js
@@ -1,11 +1,11 @@
 import MainLayout from "@/layout/MainLayout";
-import styles from '@/styles/CatalogBagsDesktopMen.module.css'
+import styles from '@/styles/CatalogBagsDesktopWomen.module.css'
 import React, {useContext, useEffect, useRef, useState} from "react";
 import {observer} from "mobx-react-lite";
 import Image from "next/image";
 import arrow from "@/static/icons/arrowSlider.svg";
 
-const CatalogBagsDesktopMen = () => {
+const Bags_desktop_women = () => {
     const linksBrands = [
         'https://sellout.su/brand1', 'https://sellout.su/brand2', 'https://sellout.su/brand3',
         'https://sellout.su/brand4', 'https://sellout.su/brand5', 'https://sellout.su/brand6',
@@ -18,6 +18,13 @@ const CatalogBagsDesktopMen = () => {
         'https://sellout.su/brand25', 'https://sellout.su/brand26', 'https://sellout.su/brand27',
         'https://sellout.su/brand28', 'https://sellout.su/brand1', 'https://sellout.su/brand2', 'https://sellout.su/brand3',
         'https://sellout.su/brand4', 'https://sellout.su/brand5', 'https://sellout.su/brand6',
+        'https://sellout.su/brand7', 'https://sellout.su/brand8', 'https://sellout.su/brand9',
+        'https://sellout.su/brand10', 'https://sellout.su/brand11', 'https://sellout.su/brand12',
+        'https://sellout.su/brand13', 'https://sellout.su/brand14', 'https://sellout.su/brand15',
+        'https://sellout.su/brand16', 'https://sellout.su/brand17', 'https://sellout.su/brand18',
+        'https://sellout.su/brand19', 'https://sellout.su/brand20', 'https://sellout.su/brand21',
+        'https://sellout.su/brand22', 'https://sellout.su/brand23', 'https://sellout.su/brand24',
+        'https://sellout.su/brand25', 'https://sellout.su/brand26', 'https://sellout.su/brand5', 'https://sellout.su/brand6',
         'https://sellout.su/brand7', 'https://sellout.su/brand8', 'https://sellout.su/brand9',
         'https://sellout.su/brand10', 'https://sellout.su/brand11', 'https://sellout.su/brand12',
         'https://sellout.su/brand13', 'https://sellout.su/brand14', 'https://sellout.su/brand15',
@@ -39,7 +46,10 @@ const CatalogBagsDesktopMen = () => {
         '11000 лотов', '12000 лотов', '13000 лотов', '14000 лотов', '15000 лотов',
         '16000 лотов', '17000 лотов', '18000 лотов', '19000 лотов', '20000 лотов',
         '21000 лотов', '22000 лотов', '23000 лотов', '24000 лотов', '25000 лотов',
-        '26000 лотов'
+        '26000 лотов', '8000 лотов', '9000 лотов', '10000 лотов',
+        '11000 лотов', '12000 лотов', '13000 лотов', '14000 лотов', '15000 лотов',
+        '16000 лотов', '17000 лотов', '18000 лотов', '19000 лотов', '20000 лотов',
+        '21000 лотов', '22000 лотов', '23000 лотов', '24000 лотов', '25000 лотов',
     ];
 
     const scrollableContainerRef = useRef(null);
@@ -102,11 +112,11 @@ const CatalogBagsDesktopMen = () => {
                 <div className={styles.brandsSection}>
                     <div className={styles.brandsTitle}>ПОПУЛЯРНЫЕ БРЕНДЫ</div>
                     <div className={styles.brandsGrid} ref={scrollableContainerRef}>
-                        {Array.from({length: 54}).map((_, idx) => (
+                        {Array.from({length: 72}).map((_, idx) => (
                             <div key={idx} className={styles.brandCircle}>
                                 <div className={styles.circle}>
                                     <Image
-                                        src={`/Images New Frontend/Desktop/Men/Products/Bags/Brands/${idx + 1}.png?v=${Date.now()}`}
+                                        src={`/Images New Frontend/Desktop/Women/Products/Bags/Brands/${idx + 1}.png?v=${Date.now()}`}
                                         alt="Brand Image"
                                         className={styles.circleImage}
                                         width={700}
@@ -135,10 +145,10 @@ const CatalogBagsDesktopMen = () => {
                 <div className={styles.categoriesSection}>
                     <div className={styles.categoriesTitle}>КАТЕГОРИИ</div>
                     <div className={styles.categoriesGrid}>
-                        {['Все сумки', 'Сумки на пояс', 'Рюкзаки', 'Спортивные', 'Кошельки', 'Клатчи', 'Кардхолдеры', 'Косметички', 'Портфели', 'Чемоданы', 'Обложки'].map((category, idx) => (
+                        {['Все сумки', 'Сумки на пояс', 'Рюкзаки', 'Сумки тоуты', 'Сумки хобо', 'Сумки вёдра', 'Спортивные', 'Кошельки', 'Клатчи', 'Кардхолдеры', 'Косметички', 'Портфели', 'Чемоданы', 'Обложки'].map((category, idx) => (
                             <div key={idx} className={styles.categoryItem}>
                                 <Image
-                                    src={`/Images New Frontend/Desktop/Men/Products/Bags/Categories/${idx + 1}.png?v=${Date.now()}`}
+                                    src={`/Images New Frontend/Desktop/Women/Products/Bags/Categories/${idx + 1}.png?v=${Date.now()}`}
                                     alt={category}
                                     width={700}
                                     height={700}
@@ -156,4 +166,4 @@ const CatalogBagsDesktopMen = () => {
     );
 };
 
-export default observer(CatalogBagsDesktopMen);
+export default observer(Bags_desktop_women);
